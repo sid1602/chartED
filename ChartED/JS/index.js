38848cd0ca940773b292a76168a27d2cc30d2384
AshwinKumarVijay <ashwin.k.v89@gmail.com>
2015-10-01 18:13:07 -0500

    Added Draft Site.

---
+++ ChartED/JS/index.js
@@ -0,0 +1,26 @@
+var readyInteractive = false;
+var readyInteractiveMenu = false;
+var readyInteractiveWindow = false;
+
+var GridCells = [];
+
+$(document).ready(function () {
+    // Set up the fullpage.js
+    //  Set the scrollbar to be shown.
+    $('#fullpage').fullpage({
+        scrollBar: true
+    });
+
+    $('#floatingWindowRightDisplayMain').perfectScrollbar();
+
+
+});
+
+
+$(window).load(function () {
+
+});
+
+$(window).resize(function () {
+
+});
